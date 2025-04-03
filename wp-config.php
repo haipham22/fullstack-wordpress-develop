44559e2a4a494140a221551290cd61ded50761f8
haipham22 <contact@haipham.net>
2025-04-03 14:37:18 +0700

    add environment configuration and update wp-config.php for database settings

--- wp-config.php
+++ wp-config.php
@@ -16,20 +16,27 @@
  * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/
  *
  * @package WordPress
- */
+*/
+
+if (!function_exists('getenv_docker')) {
+function getenv_docker($env, $default) {
+	$value = getenv($env);
+	return $value !== false ? $value : $default;
+}
+}
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress' );
+define( 'DB_NAME', !!getenv_docker('DB_NAME', 'wordpress') );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', !!getenv_docker('DB_USER', '') );
 
 /** Database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', !!getenv_docker('DB_PASSWORD', '') );
 
 /** Database hostname */
-define( 'DB_HOST', 'host.docker.internal' );
+define( 'DB_HOST', !!getenv_docker('DB_HOST', '') );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
