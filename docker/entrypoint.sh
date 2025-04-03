ceec56dcbe36aacc0904b39532573225d05ccccb
haipham22 <contact@haipham.net>
2025-04-03 13:32:12 +0700

    update docker-compose and entrypoint to use wp core download

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,10 +4,10 @@
 WP_PATH="/app"
 
 # Kiểm tra nếu thư mục WordPress chưa có, thì tải về
-if [ ! -f "$WP_PATH/wp-config.php" ]; then
-    echo "WordPress chưa được cài đặt. Đang tải..."
-    wp core download --path="$WP_PATH" --allow-root
-fi
+# if [ ! -f "$WP_PATH/wp-config.php" ]; then
+#     echo "WordPress chưa được cài đặt. Đang tải..."
+#     wp core download --path="$WP_PATH" --allow-root
+# fi
 
 # Chạy lệnh mặc định của container
 exec "$@"
