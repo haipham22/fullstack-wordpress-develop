32993cc2d9e27f5e96aae26d124edf2dbf399f91
haipham22 <contact@haipham.net>
2025-04-24 19:38:15 +0700

    Remove unused SASS variables and stylesheets, including forms, spacings, typography, and various view templates. Delete SVG icons and other template parts to streamline the theme structure and improve maintainability.

--- web/app/themes/air-light/js/src/modules/navigation/add-multiple-event-listeners.js
+++
@@ -1,6 +0,0 @@
-// Event listener helper function
-function addMultipleEventListeners(element, events, handler) {
-  events.forEach((e) => element.addEventListener(e, handler));
-}
-
-export default addMultipleEventListeners;
